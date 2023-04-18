7692d433ab21cd8a1561f7b84b3a1df416b1ea5a
Bibek Acharya <acharya.bbk@gmail.com>
2023-04-18 16:52:38 +0000

    generated the random card

--- src/app.js
+++ src/app.js
@@ -7,20 +7,26 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //1- spade, 2-clubs,
-  const shape = [1, 2, 3, 4];
-  const number = ["Ace", 2, 3, 4, 5, 6, 7, 8, 9, 10, "Jack", "Queen", "King"];
+  const shape = ["♠", "♣", "♥", "♦"];
+  const number = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
   let generatedShape = Math.floor(Math.random() * shape.length);
   let generatedNumebr = Math.floor(Math.random() * number.length);
 
   let cardIcon = document.querySelectorAll(".card-icon");
-  cardIcon[1].innerHTML = shape[generatedShape];
-  cardIcon[0].innerHTML = shape[generatedShape];
-
-  if (shape[generatedShape] === 1) {
-    cardIcon[1].style.color = "red";
-    cardIcon[0].classList.add("text-danger");
-  }
 
   let cardNumebr = document.querySelector(".card-number");
-  cardNumebr.innerHTML = shape[generatedNumebr];
+  cardNumebr.innerHTML = number[generatedNumebr];
+  cardIcon[1].innerHTML = `<h1>${shape[generatedShape]}</h1>`;
+  cardIcon[0].innerHTML = `<h1>${shape[generatedShape]}</h1>`;
+
+  if (shape[generatedShape] === "♥" || shape[generatedShape] === "♦") {
+    //cardIcon[1].style.color = "red";
+    cardIcon[0].classList.add("text-danger");
+    cardIcon[1].classList.add("text-danger");
+    cardNumebr.classList.add("text-danger");
+  } else {
+    cardIcon[0].classList.add("text-dark");
+    cardIcon[1].classList.add("text-dark");
+    cardNumebr.classList.add("text-dark");
+  }
 };
